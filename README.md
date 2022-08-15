847a150a1e7d2cfe862e931987401223b05a4d9f
Hyunho Cho <mug896@gmail.com>
2022-08-15 20:32:26 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# uftrace-bash-completion
- A bash script to complete a command line of uftrace
+## Iptables Bash Completion
+
+Copy contents of `httpie-bash-completion.sh` file to `~/.bash_completion`.  
+open new terminal and try auto completion !
+
+
+```sh
+bash$ hostnamectl
+Operating System: Ubuntu 22.04.1 LTS
+          Kernel: Linux 5.15.0-43-generic
+    Architecture: x86-64
+
+bash$ uftrace --version
+uftrace v0.9.4 ( dwarf python tui perf sched dynamic )
+
+bash$ uftrace --[tab]
+--Event              --flame-graph        --no-comment         --signal
+--argument           --flat               --no-event           --size-filter
+--auto-args          --force              --no-libcall         --sort
+--avg-self           --graphviz           --no-merge           --sort-column
+--avg-total          --help               --no-pager           --srcline
+--buffer             --host               --no-pltbind         --symbols
+--caller-filter      --keep-pid           --no-randomize-addr  --task
+. . .
+```
+
---
+++ uftrace-bash-completion.sh
@@ -0,0 +1,54 @@
+_uftrace()
+{
+    local CMD=$1 CUR=$2 PREV=$3
+    [[ $PREV = "=" ]] && PREV=${COMP_WORDS[COMP_CWORD-2]}
+    local IFS=$' \t\n' WORDS OPTS=() OPT1=() OPT2=() TMP i
+
+    IFS=$'\n'
+    for TMP in $( $CMD --help | sed -En '/^\s+-/{ s/^\s{,15}((-\w),?\s)?(--[[:alnum:]-]+=?)?.*/ \2 \3/p }')
+    do
+        if [[ $TMP =~ "=" ]]; then
+            TMP=${TMP/=/} OPT1+=( ${TMP// /$'\n'} )
+        else
+            OPT2+=( ${TMP// /$'\n'} )
+        fi
+    done
+    unset IFS
+
+    OPTS=( "${OPT1[@]}" "${OPT2[@]}" )
+    if [[ $CUR =~ ^-- ]]; then
+        WORDS=${OPTS[@]/#-[^-]*/}
+    elif [[ $CUR =~ ^- ]]; then
+        WORDS=${OPTS[@]/#--*/}" -?"
+    else
+        if [[ $PREV = @(-d|--data|--diff|-L|--libmcount-path) ]]; then
+	        COMPREPLY=( $(compgen -d -- "$CUR") )
+            return
+        elif [[ $PREV = --color ]]; then
+            WORDS="yes no auto"
+        elif [[ $PREV = --demangle ]]; then
+            WORDS="full simple no"
+        elif [[ $PREV = --match ]]; then
+            WORDS="regex glob"
+        elif [[ $PREV = @(-s|--sort) ]]; then
+            WORDS="total self call avg min max"
+        else
+            WORDS="record replay live report info dump recv graph script tui"
+            if printf '%s\n' "${OPT1[@]}" | grep -xq -- "$PREV" ||
+                [[ $PREV = @(,|@) ]] || [[ $CUR = @(,|@) ]]; then
+                WORDS=""
+            else
+                for (( i = 1; i < ${#COMP_WORDS[@]}; i++ )); do
+                    for TMP in $WORDS; do
+                        [[ ${COMP_WORDS[i]} = $TMP ]] && WORDS=""
+                    done
+                done
+            fi
+        fi
+    fi
+    COMPREPLY=( $(compgen -W "$WORDS" -- "$CUR") )
+    [ "${COMPREPLY: -1}" = "=" ] && compopt -o nospace
+}
+
+complete -o default -o bashdefault -F _uftrace uftrace
+
