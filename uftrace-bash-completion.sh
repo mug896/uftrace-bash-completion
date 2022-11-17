66d2a0f3f89b53ef3bb125f2feb35de9dbbbb90b
Hyunho Cho <mug896@gmail.com>
2022-11-18 02:16:05 +0900

    update

--- uftrace-bash-completion.sh
+++ uftrace-bash-completion.sh
@@ -1,52 +1,52 @@
 _uftrace()
 {
-    local CMD=$1 CUR=${COMP_WORDS[COMP_CWORD]} PREV=${COMP_WORDS[COMP_CWORD-1]}
-    [[ ${COMP_LINE:COMP_POINT-1:1} = " " ]] && CUR=""
-    [[ $PREV == "=" ]] && PREV=${COMP_WORDS[COMP_CWORD-2]}
-    local IFS=$' \t\n' WORDS OPTS=() OPT1=() OPT2=() TMP i
+    local cmd=$1 cur=${COMP_WORDS[COMP_CWORD]} prev=${COMP_WORDS[COMP_CWORD-1]}
+    [[ ${COMP_LINE:COMP_POINT-1:1} = " " ]] && cur=""
+    [[ $prev == "=" ]] && prev=${COMP_WORDS[COMP_CWORD-2]}
+    local IFS=$' \t\n' words opts=() opt1=() opt2=() tmp i
 
     IFS=$'\n'
-    for TMP in $( $CMD --help | sed -En '/^\s+-/{ s/^\s{,15}((-\w),?\s)?(--[[:alnum:]-]+=?)?.*/ \2 \3/p }')
+    for tmp in $( $cmd --help | sed -En '/^\s+-/{ s/^\s{,15}((-\w),?\s)?(--[[:alnum:]-]+=?)?.*/ \2 \3/p }')
     do
-        if [[ $TMP == *=* ]]; then
-            TMP=${TMP/=/} OPT1+=( ${TMP// /$'\n'} )
+        if [[ $tmp == *"="* ]]; then
+            tmp=${tmp/=/} opt1+=( ${tmp// /$'\n'} )
         else
-            OPT2+=( ${TMP// /$'\n'} )
+            opt2+=( ${tmp// /$'\n'} )
         fi
     done
     unset -v IFS
 
-    OPTS=( "${OPT1[@]}" "${OPT2[@]}" )
-    if [[ $CUR == --* ]]; then
-        WORDS=${OPTS[@]/#-[^-]*/}
+    opts=( "${opt1[@]}" "${opt2[@]}" )
+    if [[ $cur == --* ]]; then
+        words=${opts[@]/#-[^-]*/}
     elif [[ $CUR == -* ]]; then
-        WORDS=${OPTS[@]/#--*/}" -?"
-    elif [[ $PREV == @(-!(-*)L|--libmcount-path) ]]; then
+        words=${opts[@]/#--*/}" -?"
+    elif [[ $prev == @(-!(-*)L|--libmcount-path) ]]; then
         compopt -o dirnames
-    elif [[ $PREV == --color ]]; then
-        WORDS="yes no auto"
-    elif [[ $PREV == --demangle ]]; then
-        WORDS="full simple no"
-    elif [[ $PREV == --match ]]; then
-        WORDS="regex glob"
-    elif [[ $PREV == @(-!(-*)s|--sort) ]]; then
-        WORDS="total self call avg min max"
-    elif [[ $PREV == --logfile ]]; then
+    elif [[ $prev == --color ]]; then
+        words="yes no auto"
+    elif [[ $prev == --demangle ]]; then
+        words="full simple no"
+    elif [[ $prev == --match ]]; then
+        words="regex glob"
+    elif [[ $prev == @(-!(-*)s|--sort) ]]; then
+        words="total self call avg min max"
+    elif [[ $prev == --logfile ]]; then
         :
     else
-        WORDS="record replay live report info dump recv graph script tui"
-        if printf '%s\n' "${OPT1[@]}" | grep -xq -- "$PREV" ||
-            [[ $PREV == @(,|@) ]]; then WORDS=""
+        words="record replay live report info dump recv graph script tui"
+        if printf '%s\n' "${opt1[@]}" | grep -xq -- "$prev" ||
+            [[ $prev == [,@] ]]; then words=""
         else
             for (( i = 1; i < ${#COMP_WORDS[@]}; i++ )); do
-                for TMP in $WORDS; do
-                    [[ ${COMP_WORDS[i]} == $TMP ]] && WORDS=""
+                for tmp in $words; do
+                    [[ ${COMP_WORDS[i]} == $tmp ]] && words=""
                 done
             done
         fi
     fi
-    [[ $COMP_WORDBREAKS == *$CUR* ]] && CUR=""
-    COMPREPLY=( $(compgen -W "$WORDS" -- "$CUR") )
+    [[ $cur == "=" ]] && cur=""
+    COMPREPLY=( $(compgen -W "$words" -- "$cur") )
     [ "${COMPREPLY: -1}" = "=" ] && compopt -o nospace
 }
 
